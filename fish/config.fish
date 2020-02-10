a473e29b534251567fc7a59e037a61a0db4435db
Malte Sönnichsen <s.malte95@gmail.com>
2020-02-10 17:02:02 +0100

    switched from omf to fisher

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,27 @@
-if test -f ~/.nix-profile/etc/profile.d/nix.sh
+# if command --search starship > /dev/null
+#     starship init fish | source
+# end
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+if test -f ~/.nix-profile/etc/profile.d/nix.sh 
+and functions -q bass
     bass source ~/.nix-profile/etc/profile.d/nix.sh
 end
-
+    
 set -x IPYTHONDIR ~/.config/ipython
 
 if status is-interactive
 and not set -q TMUX
     exec tmux new -A -s $USER
 end
+
+if test -f ~/miniconda3/bin/conda
+    # >>> conda initialize >>>
+    # !! Contents within this block are managed by 'conda init' !!
+    eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+    # <<< conda initialize <<<
+end
+
