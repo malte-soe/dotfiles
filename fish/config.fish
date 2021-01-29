6b6edc8ca29de6631d00da64669c241575c4ae81
Malte Sönnichsen <malte-soe@users.noreply.github.com>
2021-01-29 10:30:59 +0100

    Cleanup

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,6 @@ set fish_greeting
 
 set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
 
-if test -f ~/.nix-profile/etc/profile.d/nix.sh
-and functions -q bass
-    bass source ~/.nix-profile/etc/profile.d/nix.sh
-end
 
 if status is-interactive
 and not set -q TMUX
@@ -20,12 +16,5 @@ end
 
 if command --search direnv > /dev/null
     eval (direnv hook fish)
-end
-
-if test -f ~/miniconda3/bin/conda
-    # >>> conda initialize >>>
-    # !! Contents within this block are managed by 'conda init' !!
-    eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-    # <<< conda initialize <<<
 end
 
