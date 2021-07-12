5347432db64efb111372079761583647769c99cd
Malte Sönnichsen <8942015+malte-soe@users.noreply.github.com>
2021-07-12 14:33:31 +0200

    changed stuff

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,13 @@ set fish_greeting
 
 set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
 
-
+# no login shell
 if status is-interactive
+# not already in tmux session
 and not set -q TMUX
-    exec tmux -u new -A -s $USER
+    if test -z (tmux ls -f '#{==:#{session_name},$(USER)}}' -F '#{?session_attached,yes,}')
+        exec tmux -u new -A -s $USER
+    end
 end
 
 if command --search starship > /dev/null
