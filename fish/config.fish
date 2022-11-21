e8f6226c212babb79d41b52dc40fc7b3d682bfeb
Malte Sönnichsen <8942015+malte-soe@users.noreply.github.com>
2022-11-21 12:18:05 +0100

    set editor

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,19 @@ set fish_greeting
 
 set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
 
-# no login shell
-if status is-interactive
-# not already in tmux session
-and not set -q TMUX
-    if test -z (tmux ls -f '#{==:#{session_name},$(USER)}}' -F '#{?session_attached,yes,}')
-        exec tmux -u new -A -s $USER
+if command --search tmux > /dev/null
+    # no login shell
+    if status is-interactive
+    # not already in tmux session
+    and not set -q TMUX
+        if test -z (tmux ls -f '#{==:#{session_name},$(USER)}}' -F '#{?session_attached,yes,}')
+            exec tmux -u new -A -s $USER
+        end
     end
+end
+
+if command --search nvim > /dev/null
+    set -gx EDITOR nvim
 end
 
 if command --search starship > /dev/null
@@ -18,7 +24,6 @@ end
 if command --search direnv > /dev/null
     eval (direnv hook fish)
 end
-
 
 function dark
     sed -i '' 's/colors: \*light/colors: *dark/' ~/.config/alacritty/alacritty.yml; 
