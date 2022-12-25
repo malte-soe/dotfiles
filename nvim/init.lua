dda0e752a1ec5555d46a64583d3c6247bf753b21
Malte Sönnichsen <8942015+malte-soe@users.noreply.github.com>
2022-12-25 11:43:06 +0100

    feat: cmp copilot and tmux navigation

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,6 +31,8 @@ vim.cmd([[
 
 vim.keymap.set("n", "<leader>w", vim.cmd.update)
 vim.keymap.set("n", "<leader>q", vim.cmd.quit)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 
 -- lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -48,13 +50,6 @@ vim.opt.runtimepath:prepend(lazypath)
 
 require("lazy").setup({
     "tpope/vim-sleuth",
-    {
-        'github/copilot.vim',
-        config = function()
-            vim.g.copilot_no_tab_map = true
-            vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-        end,
-    },
     {
         'jose-elias-alvarez/null-ls.nvim',
         lazy = true,
@@ -124,16 +119,32 @@ require("lazy").setup({
     {
         'mfussenegger/nvim-dap-python',
         config = function()
-            require('dap-python').setup('$HOME/miniconda3/envs/ldm/bin/python')
+            require('dap-python').setup()
         end,
     },
-    'christoomey/vim-tmux-navigator',
+    {
+        'numToStr/Navigator.nvim',
+        config = function()
+            require('Navigator').setup()
+            vim.keymap.set({ 'n', 't' }, '<C-h>', '<CMD>NavigatorLeft<CR>')
+            vim.keymap.set({ 'n', 't' }, '<C-l>', '<CMD>NavigatorRight<CR>')
+            vim.keymap.set({ 'n', 't' }, '<C-k>', '<CMD>NavigatorUp<CR>')
+            vim.keymap.set({ 'n', 't' }, '<C-j>', '<CMD>NavigatorDown<CR>')
+            vim.keymap.set({ 'n', 't' }, '<C-p>', '<CMD>NavigatorPrevious<CR>')
+        end
+    },
     'tpope/vim-unimpaired',
     'tpope/vim-commentary',
     'tpope/vim-fugitive',
     {
         'lewis6991/gitsigns.nvim',
         config = function() require("gitsigns").setup({ numhl = true }) end
+    },
+    {
+        "zbirenbaum/copilot.lua",
+        config = function()
+            require("copilot").setup()
+        end,
     },
     {
         'hrsh7th/nvim-cmp',
@@ -145,12 +156,13 @@ require("lazy").setup({
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-nvim-lua',
             'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-vsnip',
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-nvim-lsp-signature-help',
             'onsails/lspkind-nvim',
             'nvim-telescope/telescope.nvim',
             'jose-elias-alvarez/null-ls.nvim',
+            "zbirenbaum/copilot.lua",
+            "zbirenbaum/copilot-cmp",
         },
         config = function()
             local on_attach = function(_, bufnr)
@@ -167,19 +179,16 @@ require("lazy").setup({
                 nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
                 nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-                nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+                nmap('gd', builtin.lsp_definitions, '[G]oto [D]efinition')
                 nmap('gr', builtin.lsp_references, '[G]oto [R]eferences')
-                nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-                nmap('<leader>td', vim.lsp.buf.type_definition, 'Type [D]efinition')
+                nmap('gi', builtin.lsp_implementations, '[G]oto [I]mplementation')
+                nmap('<leader>td', builtin.lsp_type_definitions, '[T]ype [D]efinition')
                 nmap('<leader>ds', builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
                 nmap('<leader>ws', builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
                 nmap("F", vim.lsp.buf.format, '[F]ormat')
-                nmap("<silent>]d", vim.diagnostic.goto_next)
-                nmap("<silent>[d", vim.diagnostic.goto_prev)
 
                 -- See `:help K` for why this keymap
                 nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-                nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
                 -- Lesser used LSP functionality
                 nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
@@ -195,12 +204,19 @@ require("lazy").setup({
                 end, { desc = 'Format current buffer with LSP' })
             end
             require("luasnip.loaders.from_vscode").lazy_load()
+            require("copilot_cmp").setup()
             local cmp = require('cmp')
             local lspkind = require("lspkind")
+            lspkind.init({
+                symbol_map = { Copilot = "" },
+            })
             local luasnip = require("luasnip")
             cmp.setup({
                 formatting = {
-                    format = lspkind.cmp_format({ with_text = false, maxwidth = 50 })
+                    format = lspkind.cmp_format({
+                        with_text = false,
+                        maxwidth = 50,
+                    })
                 },
                 snippet = {
                     expand = function(args)
@@ -240,7 +256,8 @@ require("lazy").setup({
                     { name = "nvim_lua" },
                     { name = "path" },
                     { name = "luasnip" },
-                    { name = "cmp-nvim-lsp-signature-help" },
+                    { name = "nvim_lsp_signature_help" },
+                    { name = "copilot" },
                 },
             })
             local capabilities = vim.lsp.protocol.make_client_capabilities()
