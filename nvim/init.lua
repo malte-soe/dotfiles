c70d67090a6f30887183b645b7867947aea6ec8a
Malte Sönnichsen <8942015+malte-soe@users.noreply.github.com>
2023-05-08 09:53:18 +0200

    feat(nvim): move to more up to date split navigation plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -194,14 +194,26 @@ require("lazy").setup({
         end,
     },
     {
-        "numToStr/Navigator.nvim",
+        "mrjones2014/smart-splits.nvim",
         config = function()
-            require("Navigator").setup()
-            vim.keymap.set({ "n", "t" }, "<C-h>", "<CMD>NavigatorLeft<CR>")
-            vim.keymap.set({ "n", "t" }, "<C-l>", "<CMD>NavigatorRight<CR>")
-            vim.keymap.set({ "n", "t" }, "<C-k>", "<CMD>NavigatorUp<CR>")
-            vim.keymap.set({ "n", "t" }, "<C-j>", "<CMD>NavigatorDown<CR>")
-            vim.keymap.set({ "n", "t" }, "<C-p>", "<CMD>NavigatorPrevious<CR>")
+            require("smart-splits").setup()
+            -- recommended mappings
+            -- resizing splits
+            vim.keymap.set('n', '<A-h>', require('smart-splits').resize_left)
+            vim.keymap.set('n', '<A-j>', require('smart-splits').resize_down)
+            vim.keymap.set('n', '<A-k>', require('smart-splits').resize_up)
+            vim.keymap.set('n', '<A-l>', require('smart-splits').resize_right)
+            -- moving between splits
+            vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
+            vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
+            vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
+            vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
+            -- swapping buffers between windows
+            vim.keymap.set('n', '<leader><leader>h', require('smart-splits').swap_buf_left)
+            vim.keymap.set('n', '<leader><leader>j', require('smart-splits').swap_buf_down)
+            vim.keymap.set('n', '<leader><leader>k', require('smart-splits').swap_buf_up)
+            vim.keymap.set('n', '<leader><leader>l', require('smart-splits').swap_buf_right)
+
         end,
     },
     "tpope/vim-unimpaired",
